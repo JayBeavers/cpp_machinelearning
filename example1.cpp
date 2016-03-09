e5b177d30cba08b235e34d2cac7faf2fc47e727a
Jay Beavers <jay@hikinghomeschoolers.org>
2016-03-09 11:28:53 -0800

    Refactor plotting logic into plotData, starting on gradient descent

--- example1.cpp
+++ example1.cpp
@@ -1,35 +1,28 @@
-#include <mlpack/core.hpp>
-#include <iostream>
-#include "gnuplot-iostream/gnuplot-iostream.h"
+#include "./example1.h"
 
 using std::cout;
 using std::endl;
 using arma::mat;
 using arma::randu;
+using arma::ones;
+using arma::zeros;
 using mlpack::data::Load;
 
 int main() {
-  Gnuplot gp;
-
   auto data = mat();
   Load("ex1data1.txt", data);
 
-  auto X = data.row(0);
-  auto y = data.row(1);
+  auto const x = data.row(0);
+  auto const y = data.row(1);
+  auto const m = y.n_cols;
 
-  auto m = y.n_cols;
+  plotData(data);
 
-  cout << X << endl;
-  cout << y << endl;
-  cout << m << endl;
+  auto const X = join_vert(ones(1, m), x);
+  auto theta = zeros(1, 2);
 
-  gp << "set title 'Food Truck Profit'\n";
-  gp << "set xlabel 'Population of City in 10,000s'\n";
-  gp << "set ylabel 'Profit in $10,000s'\n";
-  gp << "unset key\n";
-  gp << "plot " << gp.binFile1d_colmajor(data, "record");
-  gp << "with points pt 2 lc rgb 'red'\n";
-  gp << endl;
+  auto const iterations = 1500;
+  auto alpha = 0.01;
 
   return 0;
 }
---
+++ example1.h
@@ -0,0 +1,7 @@
+#include <mlpack/core.hpp>
+#include <iostream>
+#include "gnuplot-iostream/gnuplot-iostream.h"
+
+using arma::mat;
+
+void plotData(mat data);
