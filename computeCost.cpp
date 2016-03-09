b5feae14b5f63ee82fcc4b6661dc7964688ee71d
Jay Beavers <jay@hikinghomeschoolers.org>
2016-03-09 14:24:54 -0800

    Implement computeCost

--- computeCost.cpp
+++ computeCost.cpp
@@ -4,5 +4,11 @@ using std::endl;
 using arma::mat;
 
 float computeCost(mat X, mat y, mat theta) {
-  return 0.0;
+  auto const m = y.n_cols;
+  auto const prediction = X.t() * theta.t();
+  auto const delta = prediction - y.t();
+
+  float j = accu(pow(delta, 2)) / (2 * m);
+
+  return j;
 }
